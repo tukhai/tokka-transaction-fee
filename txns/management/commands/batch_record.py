8fbe0d2359e933340351c98b1744bcf88ca80882
tukhai <ntukhai@gmail.com>
2024-04-18 15:26:15 +0800

    Optimize batch recording concurrent fetch

--- txns/management/commands/batch_record.py
+++ txns/management/commands/batch_record.py
@@ -10,11 +10,24 @@ from datetime import datetime
 class Command(BaseCommand):
     help = 'Batch record transactions from Etherscan API for the previous hour'
 
-    def get_block_number_by_timestamp(self, timestamp, closest):
-        ETHERSCAN_API_URL = 'https://api.etherscan.io/api'
-        api_key = os.environ.get('API_KEY_BATCH', None)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conn_params = {
+            'host': 'localhost',
+            'port': 5432,
+            'user': os.environ.get('DATABASE_USER', None),
+            'password': os.environ.get('DATABASE_PASSWORD', None),
+            'database': os.environ.get('DATABASE_NAME', None)
+        }
+        self.etherscane_api_url = os.environ.get('ETHERSCAN_API_URL', None)
+        self.api_key = os.environ.get('API_KEY_BATCH', None)
+        self.address = os.environ.get('ADDRESS', None)
 
-        response = requests.get(f'{ETHERSCAN_API_URL}?module=block&action=getblocknobytime&timestamp={timestamp}&closest={closest}&apikey={api_key}')
+        self.step = 1000  # to ensure that each api call is <10000. Cant loop the pagination because pageNo * offset must be <10000
+        self.semaphore = asyncio.Semaphore(4)  # Limit to 4 API calls per seconds, max is 5 so leaving some buffer
+
+    def get_block_number_by_timestamp(self, timestamp, closest):
+        response = requests.get(f'{self.etherscane_api_url}?module=block&action=getblocknobytime&timestamp={timestamp}&closest={closest}&apikey={self.api_key}')
         if response.status_code == 200:
             data = response.json()
             if data['status'] == '1' and data['result']:
@@ -22,24 +35,19 @@ class Command(BaseCommand):
 
     async def fetch_transactions(self, start, end):
         print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
-        conn = await asyncpg.connect(
-            host='localhost',
-            port=5432,
-            user=os.environ.get('DATABASE_USER', None),
-            password=os.environ.get('DATABASE_PASSWORD', None),
-            database=os.environ.get('DATABASE_NAME', None)
-        )
 
         start_block = self.get_block_number_by_timestamp(start, 'before')
         end_block = self.get_block_number_by_timestamp(end, 'after')
-        step = 1000  # to ensure that each api call is <10000. Cant loop the pagination because pageNo * offset must be <10000
 
-        api_key = os.environ.get('API_KEY_BATCH', None)
-        address = os.environ.get('ADDRESS', None)
+        async with asyncpg.create_pool(**self.conn_params) as conn_pool:
+            await self.fetch_all_urls(conn_pool, start_block, end_block)
 
-        async with aiohttp.ClientSession() as session:
-            for b in range(start_block, end_block, step):
-                url = f'https://api.etherscan.io/api?module=account&action=tokentx&address={address}&startblock={b}&endblock={min(b + step - 1, end_block)}&sort=asc&apikey={api_key}'
+        print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+
+    async def fetch_url(self, conn_pool, session, url):
+        completed = False
+        while not completed:
+            async with self.semaphore:
                 async with session.get(url) as response:
                     if response.status == 200:
                         data = await response.json()
@@ -52,12 +60,26 @@ class Command(BaseCommand):
                                     int(txn['gasPrice']) * int(txn['gasUsed']) * pow(10, -18)
                                 ) for txn in data['result']
                             ]
-                            await conn.executemany('''
-                                INSERT INTO transaction_batch_record (hash, block_number, timestamp, fee)
-                                VALUES ($1, $2, $3, $4)
-                                ON CONFLICT (hash, timestamp) DO NOTHING
-                            ''', transactions)
-        print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+                            async with conn_pool.acquire() as connection:
+                                async with connection.transaction():
+                                    await connection.executemany('''
+                                        INSERT INTO transaction_batch_record (hash, block_number, timestamp, fee)
+                                        VALUES ($1, $2, $3, $4)
+                                        ON CONFLICT (hash, timestamp) DO NOTHING
+                                    ''', transactions)
+                            completed = True
+                        else:
+                            print('data ::', data)
+            #         else:
+            #             print('response ::', response)
+
+    async def fetch_all_urls(self, conn_pool, start_block, end_block):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for b in range(start_block, end_block, self.step):
+                url = f'{self.etherscane_api_url}?module=account&action=tokentx&address={self.address}&startblock={b}&endblock={min(b + self.step - 1, end_block)}&sort=asc&apikey={self.api_key}'
+                tasks.append(asyncio.create_task(self.fetch_url(conn_pool, session, url)))
+            return await asyncio.gather(*tasks)
 
     def add_arguments(self, parser):
         parser.add_argument('start_timestamp', type=int, help='Start timestamp')
